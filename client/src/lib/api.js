1f0afbb0ba01e77fd2161816dea8d80c0f5e2ebc
Pedro Martin <pataruco@gmail.com>
2020-11-03 11:12:37 +0000

    deployment ready

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,25 +1,18 @@
+const { REACT_APP_SERVER_URL } = process.env;
+
 export const getAllFilm = async () => {
   try {
-    const response = await fetch('http://localhost:5000/film');
+    const response = await fetch(`${REACT_APP_SERVER_URL}/film`);
     const data = await response.json();
     console.log(response);
     return data;
   } catch (error) {
     console.log(error);
   }
-  //   const getFilm = async (id) => {
-  //     try {
-  //       const response = await fetch(`/films${URL}`);
-  //       const { data } = await response.json();
-  //       return data;
-  //     } finally {
-  //     }
-  //   };
-  // };
 };
 export const getAllDirectors = async () => {
   try {
-    const response = await fetch('http://localhost:5000/director');
+    const response = await fetch(`${REACT_APP_SERVER_URL}/director`);
     const data = await response.json();
     console.log(response);
     return data;
@@ -27,7 +20,7 @@ export const getAllDirectors = async () => {
 };
 
 export const createFilm = async (film) => {
-  const response = await fetch('http://localhost:5000/film', {
+  const response = await fetch(`${REACT_APP_SERVER_URL}/film`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -39,7 +32,7 @@ export const createFilm = async (film) => {
 };
 
 export const deleteFilm = async (id) => {
-  await fetch(`http://localhost:5000/film/${id}`, {
+  await fetch(`${REACT_APP_SERVER_URL}/film/${id}`, {
     method: 'DELETE',
   });
 };
